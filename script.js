a7c886cf77adf9176c5e6e4ea37dde32e1a8a6b9
kkunal626 <143017943+kkunal626@users.noreply.github.com>
2023-09-02 12:08:27 +0530

    Animation Changes

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+window.addEventListener('scroll',reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal')
+
+    for(var i = 0; i <reveals.length; i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
+
+const myText = new SplitType('.name');
+gsap.to('.char',{
+    y: 0,
+    stagger: 0.05,   //Offset 
+    delay: 0.2,
+    duratio: .1
+})
